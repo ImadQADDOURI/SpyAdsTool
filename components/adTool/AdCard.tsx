bcc8cbed4f2d37a5436d522333b92b1547bbf30d
JS-Debug <imad.qaddouri@ump.ac.ma>
2025-08-28 18:43:29 +0100

    test

--- components/adTool/AdCard.tsx
+++ components/adTool/AdCard.tsx
@@ -139,7 +139,7 @@ const AdCard: React.FC<AdCardProps> = memo(({ ad, compact = false }) => {
     <Card
       className={`relative flex h-fit w-full max-w-lg flex-col overflow-hidden transition-shadow duration-300 ${
         isHotAd
-          ? "shadow-[0_0_10px_2px_rgba(148,0,211,0.5)] dark:shadow-[0_0_10px_2px_rgba(148,0,211,0.85)]"
+          ? "shadow-[0_0_10px_1px_rgba(148,0,211,0.5)] dark:shadow-[0_0_10px_1px_rgba(148,0,211,0.85)]"
           : "shadow-lg hover:shadow-xl"
       }`}
     >
